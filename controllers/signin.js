b41382e60cb536ea2d754043b559609d6fc76e0d
Endrit <endrit.books@gmail.com>
2021-05-17 06:06:48 +0200

    optimised better

--- controllers/signin.js
+++ controllers/signin.js
@@ -13,7 +13,7 @@ const handleSignin = (db, bcrypt) => (req, res) => {
 	  		  .then(user => {
 	  		  	res.json(user[0])
 	  		  })
-	  		  .catch(err => res.status(400).json('unable to get user!!!'))
+	  		  .catch(err => res.status(400).json('unable to get user!'))
 	  	} else {
 	  		res.status(400).json('wrong credentials')
 	  	}
